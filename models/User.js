5108b901f81872ac6f45858e0fedde7dbb5e4571
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-05-06 10:53:04 +0100

    :clown: Re-add models folder and User.js in correct place :clown:

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.index({
+  name: 1,
+  email: 1
+}, {
+  unique: true
+});
+
+module.exports = User = mongoose.model('users', userSchema);

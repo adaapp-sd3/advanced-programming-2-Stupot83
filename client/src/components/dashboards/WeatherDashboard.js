90ce7e03d59aee8b13a10d56ba604f573a231dc4
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-05-11 17:33:14 +0100

    :construction: Initial work to set up weather dashboard :construction:

---
+++ client/src/components/dashboards/WeatherDashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './WeatherDashboard.css';
+
+class WeatherDashboard extends Component {
+    render() {
+      return (
+          <div></div>
+      )
+    }
+  }
+  
+  export default WeatherDashboard
